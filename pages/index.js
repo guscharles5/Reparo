a38349d90f5ef7988948e484ad3328aa105e2b13
guscharles5 <gus.charles5a@gmail.com>
2026-04-30 23:33:52 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import dynamic from 'next/dynamic'
 
-const ReparoApp = dynamic(() => import('../components/ReparoApp'), { ssr: false })
+const ReparoApp = dynamic(() => import('../components/ReparoApp.jsx'), { ssr: false })
 
 export default function Home() {
   return <ReparoApp />
