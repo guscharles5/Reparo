84dba588f56304d7541e8d258b547fbbd8cd3367
guscharles5 <gus.charles5a@gmail.com>
2026-05-05 01:38:14 +0200

    Delete pages/index.js

--- pages/index.js
+++
@@ -1,7 +0,0 @@
-import dynamic from 'next/dynamic'
-
-const ReparoApp = dynamic(() => import('../components/ReparoApp.jsx'), { ssr: false })
-
-export default function Home() {
-  return <ReparoApp />
-}
